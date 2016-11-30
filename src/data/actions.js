312a023a45ec800cc0fb849c741c5bfcdc8aed5e
ShMcK <shawn.j.mckay@gmail.com>
2016-11-29 21:22:06 -0800

    load mock posts

--- src/data/actions.js
+++ src/data/actions.js
@@ -1,3 +1,4 @@
 /** 
  * List of Redux Actions
  */
+export const POSTS_LOAD = 'POSTS_LOAD'
--- src/data/modules/post/index.js
+++ src/data/modules/post/index.js
@@ -6,10 +6,34 @@
  * voteUp Post
  * sort Posts
  * add Post Comment
+ * 
+ * @flow
  */
 
+// fetch polyfill that works with node tests
+import fetch from 'isomorphic-fetch'
+import { POSTS_LOAD } from '../../actions'
+
+export const postsLoad = () => dispatch => {
+  // HackerNews API docs: https://github.com/HackerNews/API
+  fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+    .then(data => data.json())
+    .then((postIds: number[]) => {
+      // TODO: figure out how to limit Firebase queries
+      const posts = postIds.split(0, 10)
+      dispatch({ type: POSTS_LOAD, payload: { posts } })
+    })
+}
+
 const reducer = (state = {}, action) => {
-  return state
+  switch (action.type) {
+
+    case POSTS_LOAD:
+      return action.payload.posts
+      
+    default:
+      return state
+  }
 }
 
 export default reducer
