aa404feeca975f8638afdee51845e5f9c6e994db
ShMcK <shawn.j.mckay@gmail.com>
2016-11-30 20:23:18 -0800

    style login card

--- src/containers/AuthScreen/index.js
+++ src/containers/AuthScreen/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, TouchableHighlight } from 'react-native'
 // import { connect } from 'react-redux'
 import styles from './styles'
+import Header from '../../components/Header'
 
 class AuthScreen extends Component {
   static route = {
@@ -9,10 +10,46 @@ class AuthScreen extends Component {
       title: 'Login',
     },
   }
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Auth</Text>
+
+        <View style={styles.top}>
+          <Header />
+        </View>
+
+        <View style={[styles.card, styles.shadow]}>
+
+          <View style={styles.input}>
+            <TextInput
+              style={styles.inputText}
+              placeholder='email'
+              onChangeText={email => this.setState({ email })}
+              value={this.state.email} />
+          </View>
+          
+          <View style={styles.divider} />
+
+          <View style={styles.input}>
+          <TextInput
+            style={styles.inputText}
+            placeholder='password'
+            onChangeText={password => this.setState({ password })}
+            value={this.state.password} />
+          </View>
+
+          <TouchableHighlight style={[styles.submit, styles.shadow]}>
+            <Text style={styles.submitText}>Login</Text>
+          </TouchableHighlight>
+
+        </View>
       </View>
     )
   }
--- src/containers/AuthScreen/styles.js
+++ src/containers/AuthScreen/styles.js
@@ -3,7 +3,67 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
     backgroundColor: 'lightgrey',
+  },
+  top: {
+    flex: 5,
+  },
+  card: {
+    backgroundColor: 'white',
+    flex: 6,
+    flexDirection: 'column',
+    marginRight: 8,
+    marginLeft: 8,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    padding: 30,
+    paddingTop: 80,
+  },
+  shadow: {
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+  },
+  input: {
+    
+  },
+  inputText: {
+    height: 35,
+    paddingLeft: 8,
+    paddingRight: 8,
+    paddingTop: 4,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  divider: {
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: -30,
+    marginRight: -30,
+    borderWidth: 0.5,
+    borderStyle: 'solid',
+    borderColor: 'grey',
+  },
+  submit: {
+    backgroundColor: 'blue',
+    marginTop: 20,
+    marginLeft: 30,
+    marginRight: 30,
+    borderRadius: 10,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  submitText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 })
 
